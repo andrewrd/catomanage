f4004b2d62f5e91298400919412248ef1aefd62e
Sean <gilmore.seven@gmail.com>
2016-10-13 20:23:34 +1100

    Added new file editcat.php

---
+++ pages/editcat.php
@@ -0,0 +1,10 @@
+<?php
+//Includes the application header
+include 'layouts/header.php';
+//Allows function calls and connection database
+include 'controller/controller.php';
+ 
+
+//includes the footer of the application
+include 'layouts/footer.php';
+?>
