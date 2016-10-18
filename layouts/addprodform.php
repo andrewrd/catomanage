b49e1b93de65b35a3126d440167508595e9258c4
Sean <gilmore.seven@gmail.com>
2016-10-18 21:13:39 +1100

    added addproduct form template, function stubs for handling form submission for adding a product

---
+++ layouts/addprodform.php
@@ -0,0 +1,6 @@
+<!--this form deals with adding new product information to the database-->
+<h3 class="text-center">Add New Product</h3>
+<form>
+  <input name="prod_name">
+  <button type="submitt">Submit</button>
+</form>
