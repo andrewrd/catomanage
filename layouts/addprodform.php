679e82c9986ea4d564f509a1a1c31a6179e5eafd
Sean <gilmore.seven@gmail.com>
2016-10-20 12:55:02 +1100

    reformatted addprodform.php, added method and action properties to form

--- layouts/addprodform.php
+++ layouts/addprodform.php
@@ -2,95 +2,70 @@
 <div class="container">
     <!-- Main content starts here -->
 
-    <form class="form-horizontal">
+    <form action="addprod.php" method="POST" class="form-horizontal">
 
         <div class="form-group">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 header">
                 <h3 class="title">Add Product</h3>
 
-
-
             </div>
         </div>
         <section data-scroll-index="1">
             <div class="form-group">
-
                 <div class="col-xs-12 col-sm-12 col-md-13 col-lg-12">
                     <h3 class="subHeader">Product Details</h3>
-
-
                 </div>
                 <!--Product Name Info-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Product Name</p>
-
                 </div>
-
                 <div class="col-xs-12 col-sm-9  col-md-3 col-lg-4">
-                    <input type="text" class="text-input-underline" placeholder="Product Name" id="form-input-productName">
+                    <input name="prod_name" type="text" class="text-input-underline" placeholder="Product Name" id="form-input-productName">
                 </div>
-
                 <!--Product name Info ends-->
             </div>
             <!--Short Description Starts-->
             <div class="form-group row">
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-
-
                     <p class="labelText">Short Description
                         <br>(Max 128 Characters)</p>
                 </div>
                 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-10">
-
                     <textarea class="form-control" id="form-input-shortDesc" rows="2" placeholder="Description Text"></textarea>
                 </div>
-
             </div>
             <!--Short Description Ends-->
             <!--Long Description Starts-->
             <div class="form-group row">
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-
                     <p class="labelText">Long Description
                         <br>(Max 256 Characters)</p>
-
-
                 </div>
                 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-10">
-
                     <textarea class="form-control exampleTextarea" id="form-input-longDesc" rows="4" placeholder="Description Text"></textarea>
                 </div>
             </div>
             <!--Long Description Ends-->
-
             <!--Add to Category Starts-->
             <div class="form-group row">
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Add to Category</p>
                 </div>
                 <div class="col-xs-12 col-sm-9 col-md-9  col-lg-10">
-
                   <?php get_all_categories($dbo); ?>
-
                 </div>
             </div>
             <!--Add to Category Ends-->
         </section>
-
-
-
         <section>
             <!--Packaging Information Starts-->
             <div class="form-group">
-
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Packaging Dimensions</p>
-
                 </div>
                 <!--Length Label-->
                 <div class="hidden-xs col-sm-1 col-md-1 col-lg-1">
                     <p class="labelText">Length:</p>
-
                 </div>
                 <!--Length Input-->
                 <div class="col-xs-12 col-sm-1  col-md-1 col-lg-2">
@@ -99,7 +74,6 @@
                 <!--Width Label-->
                 <div class="hidden-xs col-sm-1 col-md-1 col-lg-1">
                     <p class="labelText">Width</p>
-
                 </div>
                 <!--Width input-->
                 <div class="col-xs-12 col-sm-2  col-md-2 col-lg-2">
@@ -107,18 +81,15 @@
                 </div>
                 <div class="hidden-xs col-sm-1 col-md-1 col-lg-1">
                     <p class="labelText">Height:</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-2  col-md-2 col-lg-2">
                     <input type="text" class="text-input-underline" placeholder="Height" id="form-height-input">
                 </div>
-
             </div>
             <div class="form-group row">
                 <!--Weight Starts Here-->
                 <div class="hidden-xs col-sm-3 col-md-2 col-lg-2">
                     <p class="labelText">Weight</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
                     <input type="text" class="text-input-underline" placeholder="Weight" id="form-weight-input">
@@ -127,20 +98,17 @@
                 <!--Product SKU Starts here-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Product SKU</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-4">
                     <input type="text" class="text-input-underline" placeholder="Product SKU" id="form-sku-input">
                 </div>
                 <!--Product SKU Ends here-->
-
             </div>
             <!--Packaging Information Ends-->
             <!--Display Product Starts-->
             <div class="form-group row">
                 <div class="hidden-xs col-sm-3 col-md-2 col-lg-2">
                     <p class="labelText">Display Product</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
                     <div class="radio">
@@ -154,27 +122,24 @@
                 </div>
             </div>
             <!--Product Display Ends-->
-
             <!--Attribute Management Starts here-->
             <div class="form-group">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <h3 class="subHeader">Attribute Management</h3>
-
                 </div>
             </div>
             <!--Current Set Attributes Starts here-->
             <div class="form-group">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <h4 class="subHeader">Current Set Attributes</h4>
-
                 </div>
             </div>
             <!--Current Set attributes ends here-->
             <div class="form-group">
                 <div class="col-xs-12 col-sm-12 col-md-13 col-lg-12">
                     <ul>
-                        <li>Colour - Values: Red(+$5), Blue(+$4), Green(+$3) X Remove</li>
-                        <li>Size - Values: Large(+$5), Medium(+$4), Small(+$3) X Remove</li>
+                      <li>Colour - Values: Red(+$5), Blue(+$4), Green(+$3) X Remove</li>
+                      <li>Size - Values: Large(+$5), Medium(+$4), Small(+$3) X Remove</li>
                     </ul>
                 </div>
             </div>
@@ -186,30 +151,24 @@
             <!--Attribute Input Starts here-->
             <!--Attribute Name-->
             <div class="form-group row">
-
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-
                     <p class="labelText">Attribute Name</p>
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
                     <input type="text" class="text-input-underline" placeholder="Attribute Name" id="form-input-attributeName">
                 </div>
-
             </div>
             <!--Attribute Value and Price Inputs-->
             <div class="form-group">
                 <!--Attribute Value Label and Input-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-
                     <p class="labelText">Attribute Value</p>
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
                     <input type="text" class="text-input-underline" placeholder="Attribute Value" id="form-input-attributeValue">
                 </div>
-
                 <!--Attribute Price Label and Input-->
                 <div class="hidden-xs col-sm-3 col-md-2 col-lg-2">
-
                     <p class="labelText">Attribute Price</p>
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
@@ -219,22 +178,18 @@
                 <div class="col-xs-3 col-sm-2 col-md-2 col-lg-12 ">
                     <input type="submit" value="Add New Value +" id="btn-addNewAttrVal" class="btn btn-default" />
                 </div>
-
             </div>
             <!--Price Management Section Starts here-->
             <div class="form-group">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <h3 class="subHeader">Price Management</h3>
-
                 </div>
             </div>
             <!--Product Base Price Label and Input-->
             <div class="form-group">
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Product Base Price</p>
-
                 </div>
-
                 <div class="col-xs-12 col-sm-9  col-md-3 col-lg-4">
                     <input type="text" class="text-input-underline" placeholder="Product Price" id="form-input-productPrice">
                 </div>
@@ -243,22 +198,22 @@
             <div class="form-group row">
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">On Special</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-3  col-md-3 col-lg-4">
                     <div class="radio">
                         <label>
-                            <input type="radio" name="optradio" id="form-input-onSpecial-yes">Yes</label>
+                            <input type="radio" name="optradio" id="form-input-onSpecial-yes">Yes
+                        </label>
                     </div>
                     <div class="radio">
                         <label>
-                            <input type="radio" name="optradio" id="form-input-dispProd-no">No</label>
+                            <input type="radio" name="optradio" id="form-input-dispProd-no">No
+                        </label>
                     </div>
                 </div>
                 <!--Special Discount Label and input-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Special Discount</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-9  col-md-3 col-lg-4">
                     <input type="text" class="text-input-underline" placeholder="Special Discount" id="form-input-specialDiscount">
@@ -268,30 +223,21 @@
             <div class="form-group row">
                 <!--Special for shopper group label-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-
                     <p class="labelText">Special for Shopper Group</p>
                 </div>
                 <!--Shopper group selection-->
                 <div class="col-xs-12 col-sm-3 col-md-3  col-lg-4">
-
-
-
                     <select multiple class="form-control" id="form-input-specialShopGroup">
-
                         <option>1. Shopper Group 1</option>
                         <option>2. Shopper Group 2</option>
                         <option>3. Shopper Group 3</option>
                         <option>4. Shopper Group 4</option>
                         <option>5. Shopper Group 5</option>
                     </select>
-
-
-
                 </div>
                 <!--Group Discount Label and Input-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Group Discount</p>
-
                 </div>
                 <div class="col-xs-12 col-sm-9  col-md-3 col-lg-4">
                     <input type="text" class="text-input-underline" placeholder="Group Discount" id="form-input-groupDiscount">
@@ -307,18 +253,14 @@
             <div class="form-group row">
                 <div class="form-group">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-                        <input type="submit" value="Add Product" id="btn-submitAddProductForm" class="btn btn-default" />
+                        <input type="submit"  id="btn-submitAddProductForm" class="btn btn-default" />
                     </div>
-
                 </div>
             </div>
         </section>
         <!--                        Supporting Documents ends-->
     </form>
 <!--Form Container Ends here-->
-
-
-
 
 </div>
 <!--Form Container ends here-->
