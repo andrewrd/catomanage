bb705145a0f5c3fa45cfa12e6fed9c1466051b4e
Tuckmo <marktuckey.art@gmail.com>
2016-11-02 10:55:46 +1100

    Fixed error where the validation javascript in the add product page was accepting + and -. Now should only accept Integers and decimals. Fixed error where long description text area was already filled with blank spaces.

--- js/validateForm.js
+++ js/validateForm.js
@@ -21,6 +21,10 @@ function isSKU(str){
     return str.match(/^[a-zA-Z0-9-]+$/)
 }
 
+function isNumber(str){
+    return str.match(/^(\d*\.)?\d+$/);
+}
+
 /*
 Function to change the colour of input fields.
 Parameters:
@@ -98,10 +102,11 @@ function checkNumber(elemId){
         var intValue = Number(nameElement.value);
         
         //if the value is not a number, correct input is false
-        if(isNaN(intValue)){
+        if(!isNumber(nameElement.value)){
             correctInput = false;
             errorMessage += "The value you entered isn't a number."
         }
+
     }
     if(!correctInput){
         changeColour(nameElement, nameElement.tagName, "red");
