770830469c0410a932ef1df71d5f69636fbe6b4d
andrewrd <andrewrd@users.noreply.github.com>
2016-11-03 23:09:37 +1100

    initialised edit_category

--- layouts/editcatform.php
+++ layouts/editcatform.php
@@ -2,13 +2,12 @@
 <div class="container">
 
     <form action="editcategory.php" method="POST" class="form-horizontal">
-            <!--Short Description Ends-->
             <div class="form-group row">
-                <!--Special for shopper group label-->
+                <!--Column left-->
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
-                    <p class="labelText">Parent Category</p>
+                    <p class="labelText">Select the category you want to edit.</p>
                 </div>
-                <!--Shopper group selection-->
+                <!--List the categories-->
                 <div class="col-xs-12 col-sm-9 col-md-9  col-lg-10">
                      <?php get_all_categories($dbo); ?>
                 </div>
@@ -16,11 +15,12 @@
     </form>
 </div>
 
+
 <!-- Form Container starts here -->
 <div class="container">
     <!-- Main content starts here -->
 
-    <form action="editcategory.php" method="POST" class="form-horizontal">
+    <form action="addcategory.php" method="POST" class="form-horizontal" enctype="multipart/form-data">
 
         <div class="form-group">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 header">
@@ -38,8 +38,16 @@
                     <p class="labelText">Category Name</p>
                 </div>
                 <div class="col-xs-12 col-sm-9  col-md-3 col-lg-4">
-                    <input name="cat_name_title" type="text" class="text-input-underline" placeholder="Category Name" id="form-input-catName" oninput="checkString(this.id, 40)" required>
-                    <p id="form-error-catName"></p>
+                    <input name="cat_name_title" type="text" class="text-input-underline" placeholder="Category Name" id="form-input-catName" oninput="checkString(this.id, 40)" required 
+                    value="<?php if(isset($_POST['cat_name_title'])){$_POST['cat_name_title'];}?>">
+                    <p id="form-error-catName">
+                        <?php
+
+                        if(isset($_POST['cat_name_error'])){
+                            echo $_POST['cat_name_error'];
+                        }   
+                        ?>
+                    </p>
                 </div>
                 <!--Product name Info ends-->
             </div>
@@ -51,7 +59,15 @@
                 </div>
                 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-10">
                     <textarea name="cat_desc" class="form-control" id="form-input-desc" rows="2" placeholder="Description Text" required oninput="checkString(this.id, 128)"></textarea>
-                    <p id="form-error-desc"></p>
+                    <p id="form-error-desc">
+                    
+                        <?php
+
+                        if(isset($_POST['cat_desc_error'])){
+                            echo $_POST['cat_desc_error'];
+                        }   
+                        ?>
+                    </p>
                 </div>
             </div>
             <!--Short Description Ends-->
@@ -63,7 +79,17 @@
                 <!--Shopper group selection-->
                 <div class="col-xs-12 col-sm-9 col-md-9  col-lg-10">
                      <?php get_all_categories($dbo); ?>
+                    <p id="form-error-desc">
+
+                        <?php
+
+                        if(isset($_POST['cat_error'])){
+                            echo $_POST['cat_error'];
+                        }   
+                        ?>
+                    </p>
                 </div>
+                
             </div>
             <!-- Add one for child category -->
 
@@ -75,37 +101,51 @@
 
            
             <div class="form-group row">
-                
+
                 <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
                     <p class="labelText">Image URL</p>
                 </div>
-                <div class="col-xs-12 col-sm-3  col-md-3 col-lg-4">
-                    <input name="cat_img_url" type="text" class="text-input-underline" placeholder="Image URL" id="form-url-input">
+                <div class="col-xs-3 col-sm-3  col-md-3 col-lg-4">
+                    <input name="cat_img_url" type="file" class="text-input-underline" placeholder="Image URL" id="form-url-input">
+                    <p id="form-error-img">
+                        <?php
+
+
+                        if(isset($_POST['cat_img_error'])){
+                            echo $_POST['cat_img_error'];
+                        }
+                        ?>
+
+
+                    </p>
+                    
                 </div>
             </div>
             <!--Packaging Information Ends-->
-            <!--Display Product Starts-->
             <div class="form-group row">
-                <div class="hidden-xs col-sm-3 col-md-2 col-lg-2">
-                    <p class="labelText">Display Category</p>
+
+                <div class="hidden-xs col-sm-3 col-md-3 col-lg-2">
+                    <p class="labelText">Display Command</p>
                 </div>
-                <div class="col-xs-12 col-sm-3  col-md-3 col-lg-3">
-                    <div class="radio">
-                        <label>
-                            <input name="cat_disp_cmd" value="yes" type="radio" name="optradio" id="form-input-dispProd-yes" required>Yes</label>
-                    </div>
-                    <div class="radio">
-                        <label>
-                            <input name="cat_disp_cmd" value="no" type="radio" name="optradio" id="form-input-dispProd-no">No</label>
-                    </div>
+                <div class="col-xs-3 col-sm-3  col-md-3 col-lg-4">
+                    <input name="cat_disp_cmd" id="form-input-dispCmd" class="text-input-underline" maxlength="128" type="text" placeholder="DisplayCategory.php" value="DisplayCategory.php" oninput="checkFilename(this.id,128 )">
+                    
+                    <p id="form-error-dispCmd">
+                        <?php
+
+
+                        if(isset($_POST['prod_disp_cmd_error'])){
+                            echo $_POST['prod_disp_cmd_error'];
+                        }
+                        ?>
+
+
+                    </p>
+                    
                 </div>
             </div>
-            <!--Product Display Ends-->
-            <!--Attribute Management Starts here-->
 
-            <!--Attribute Value and Price Inputs-->
-            
-            <!--Price Management Section Starts here-->
+
             
            
             <!--Form Submit button-->
@@ -118,6 +158,11 @@
             </div>
         </section>
     </form>
+<!--Form Container Ends here-->
+<script type="text/javascript" src="../js/validateForm.js"></script>
+</div>
+<!--Form Container ends here-->
+
 <!--Form Container Ends here-->
 <script type="text/javascript" src="../js/validateForm.js"></script>
 </div>
