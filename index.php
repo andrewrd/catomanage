4f0ed3bb5e517923174a10f895c91402471c4d07
Sean <gilmore.seven@gmail.com>
2016-11-04 11:32:12 +1100

    actually added index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'layouts/header.php'; ?>
+<div class="container">
+  <div class="jubotron">
+    <h1>COMP 344 Catalogue Management</h1>
+    <h1 class="lead">Welcome to the Catalogue Management Subsystem</h1>
+  </div>
+</div>
+<?php include 'layouts/footer.php'; ?>
